f5ea531588e7824367d44916234ab738ac3d142b
Grimhunter <62704709+Grimhunter9@users.noreply.github.com>
2021-05-24 14:21:25 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+
+
+
+
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    )
+  }
+}
+
+export default App
+
+ 
